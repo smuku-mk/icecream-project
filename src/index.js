8e0e39b5934265034e6b768996554d3d9a699ed6
Bartosz-Zarski <skulmare@gmail.com>
2023-01-26 21:18:55 +0100

    Mobile almost done, added Swapper.js, styled swapper, added swapper scss module files, added pagination standalone icon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import Swiper, { Pagination } from 'swiper';
+const swiper = new Swiper('.mySwiper', {
+  modules: [Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
