152a4869fc387143edbb0c7e0b957663670b5ba0
Mateusz Kubiak <smuku.mk@gmail.com>
2023-02-02 21:18:38 +0100

    small fixes

--- src/modal_franchise.js
+++ src/modal_franchise.js
@@ -1,15 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal_franchise-open]"),
-      closeModalBtn: document.querySelector("[data-modal_franchise-close]"),
-      modal: document.querySelector("[data-modal_franchise]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("overlay_franchise__is-hidden");
-    }
-  })();
-  
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal_franchise-open]'),
+    closeModalBtn: document.querySelector('[data-modal_franchise-close]'),
+    modal: document.querySelector('[data-modal_franchise]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('overlay_franchise__is-hidden');
+  }
+})();
