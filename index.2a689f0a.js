31e44e81310e687159e04088bdabfdb433bd93e8
SmukuHapace <104195771+SmukuHapace@users.noreply.github.com>
2023-02-02 22:32:50 +0000

    Deploying to gh-pages from @ SmukuHapace/icecream-project@5b3b7e3cbeb1e6b2eaa5a6a5800443203429faa5 🚀

---
+++ index.2a689f0a.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-franchise-open]"),closeModalBtn:document.querySelector("[data-modal-franchise-close]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("overlay-franchise__is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.2a689f0a.js.map
