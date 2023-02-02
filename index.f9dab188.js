be351049e1017628886d6c1935fd327f826129c9
SmukuHapace <104195771+SmukuHapace@users.noreply.github.com>
2023-02-02 22:53:39 +0000

    Deploying to gh-pages from @ SmukuHapace/icecream-project@437d3ee02798044606876c277338533f4cf24ae8 🚀

---
+++ index.f9dab188.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden-about")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.f9dab188.js.map
