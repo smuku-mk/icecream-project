4deaa0fa101268e7ca85738b327391f9d5f705a1
SmukuHapace <104195771+SmukuHapace@users.noreply.github.com>
2023-02-03 16:51:19 +0000

    Deploying to gh-pages from @ SmukuHapace/icecream-project@b5bccb1881f6c5cce617887f9418ec65ec519a8d 🚀

---
+++ index.2176b69b.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-locations-open]"),closeModalBtn:document.querySelector("[data-modal-locations-close]"),modal:document.querySelector("[data-modal-locations]")};function e(){o.modal.classList.toggle("overlay-locations__is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.2176b69b.js.map
---
+++ index.5da07be6.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-menu-open]"),closeModalBtn:document.querySelector("[data-modal-menu-close]"),modal:document.querySelector("[data-modal-menu]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.5da07be6.js.map
